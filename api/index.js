5c5e4b20c234572c93a93cc1a14c490fddf86c98
Achilles <solohag92@gmail.com>
2025-03-06 22:33:54 +0000

    Add init files

---
+++ api/index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import { PrismaClient } from "@prisma/client";
+
+const EXPRESS_PORT = 3001;
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "*",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.get("/leads", async (req, res) => {
+  const managers = await prisma.lead.findMany();
+  res.json(managers);
+});
+
+app.post("/leads", async (req, res) => {
+  const { name, email, status } = req.body;
+  const leadManager = await prisma.lead.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (leadManager) {
+    res.json("Email already exists");
+  } else {
+    const manager = await prisma.lead.create({
+      data: {
+        name,
+        email,
+        status,
+      },
+    });
+    res.json(manager);
+  }
+});
+
+// Handle local development vs Vercel deployment
+
+if (process.env.NODE_ENV !== "production" || !process.env.VERCEL) {
+  app.listen(EXPRESS_PORT, () => {
+    console.log(
+      `Server is running on http://localhost:${EXPRESS_PORT || 3001}`
+    );
+  });
+}
+
+// Export the app for Vercel
+export default app;
